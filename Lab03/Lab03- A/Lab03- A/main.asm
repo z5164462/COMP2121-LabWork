103fe3250d834d2574a6d3d5c4745a880a965dad
z5164462 <z5164462@ad.unsw.edu.au>
2019-03-04 15:43:01 +1100

    Loads onto the stack

--- Lab03/Lab03- A/Lab03- A/main.asm
+++ Lab03/Lab03- A/Lab03- A/main.asm
@@ -7,6 +7,39 @@
 
 
 ; Replace with your application code
+
+
+.include "m2560def.inc"
+
+
+
+.def letter = r16
+.def len = r17
+.dseg
+.org 0x200
+	rev_string: .byte 4
+
+.cseg
+	rjmp start
+	string: .db "abc",0
+
 start:
-    inc r16
-    rjmp start
+	ldi ZL, low(string<<1)	;load address of first character
+	ldi ZH, High(string<<1)
+	
+	ldi YL, low(RAMEND)		;initialise stack pointer
+	ldi YH, high(RAMEND)
+	out SPL, YL
+	out SPH, YH
+	
+	clr len
+loop_start:
+	lpm letter, Z+
+	cpi letter, 0
+	breq end_loop
+	push letter
+	inc len
+	rjmp loop_start
+
+end_loop:
+
