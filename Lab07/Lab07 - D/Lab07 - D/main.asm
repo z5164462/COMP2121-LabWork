3bf35ab67e24bea7212877e5f9776ba821356355
z5164462 <z5164462@ad.unsw.edu.au>
2019-04-03 15:26:01 +1100

    Combined lab 3 in place sorting with new register formats

--- Lab07/Lab07 - D/Lab07 - D/main.asm
+++ Lab07/Lab07 - D/Lab07 - D/main.asm
@@ -1,12 +1,383 @@
 ;
-; Lab07 - D.asm
+; Lab07 - C.asm
 ;
-; Created: 2/04/2019 1:04:56 PM
-; Author : andre
+; Created: 30/03/2019 10:59:04 PM
+; Author : Anirudh
 ;
 
 
-; Replace with your application code
-start:
-    inc r16
-    rjmp start
+
+;ASSUMPTION!!
+; No queue handling is being implemented at this stage. To move to floor 1 is just to change the current floor
+
+
+
+.include "m2560def.inc"
+.def counter = r15				; arbitrary counter
+.def row = r16					; current row
+.def col = r17					; current column
+.def rmask = r18				; mask for row during scan
+.def cmask = r19				; mask for column during scan
+.def temp = r20
+.def temp2 = r21
+.def floor = r22
+
+.equ PORTLDIR = 0xF0			; 0xF0 = 0b11110000 -> Setting PORTA 7:4 as output and 3:0 as input
+.equ INITCOLMASK = 0XEF			; 0xEF = 0b11101111 -> Mask to decide which column is selected
+.equ INITROWMASK = 0x01			; 0x01 = 0b00000001 -> Mask to check which row is selected
+.equ ROWMASK = 0x0F				; 0x0F = 0b00001111 -> To get keyboard output value using an AND operation
+.equ LCD_RS = 7
+.equ LCD_E = 6
+.equ LCD_RW = 5
+.equ LCD_BE = 4
+.equ l_one = 0b10000000
+.equ l_two = 0b11000000
+
+.dseg
+	floor_array: .BYTE 2
+
+.cseg
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// MACROS
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\\
+
+
+.macro do_lcd_command						// LCD commands
+	ldi r16, @0
+	rcall lcd_command
+	rcall lcd_wait
+.endmacro
+
+.macro do_lcd_command_reg					// LCD commands, with registers
+	mov r16, @0
+	rcall lcd_command
+	rcall lcd_wait
+.endmacro
+
+.macro change_line							// change line and cursor position on line
+	ldi r16, @0
+	cpi r16, 2
+	breq line_two
+	ldi temp2, l_one
+	ori temp2, @1
+	do_lcd_command_reg temp2
+	jmp end_cl
+line_two:
+	ldi temp2, l_two
+	ori temp2, @1
+	do_lcd_command_reg temp2
+end_cl:
+.endmacro
+
+.macro write								// write immediate data to LCD screen
+	ldi r16, @0
+	rcall lcd_data
+	rcall lcd_wait
+.endmacro
+
+.macro write_reg							// write register data to LCD screen
+	mov r16, @0
+	rcall lcd_data
+	rcall lcd_wait
+.endmacro
+
+.macro clear_disp							// clear the LCD Display
+	do_lcd_command 0b00000001
+.endmacro
+
+.macro lcd_set								// set bit in PORTA
+	sbi PORTA, @0
+.endmacro
+.macro lcd_clr								// clear bit in PORTA
+	cbi PORTA, @0
+.endmacro
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// INTERRUPTS
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\\
+
+//-----------------------------
+// |  C3   |  C2   |  C1   |  C0   |  R3   |  R2   |  R1   |  R0   |
+// |  PL0  |  PL1  |  PL2  |  PL3  |  PL4  |  PL5  |  PL6  |  PL7  |
+
+.org 0
+	jmp RESET
+
+RESET:
+	ldi temp, low(RAMEND)		; initialise stack
+	out SPL, temp
+	ldi temp, high(RAMEND)
+	out SPH, temp
+
+	ldi temp, PORTLDIR			; set ports A, C, F, G, L
+	sts DDRL, temp
+	ser temp
+	out DDRC, temp
+	out PORTC, temp
+	out DDRG, temp
+	out PORTG, temp
+	out DDRF, temp
+	out DDRA, temp
+
+	clr temp
+	out PORTF, temp
+	out PORTA, temp
+
+	do_lcd_command 0b00111000 ; 2x5x7
+	rcall sleep_5ms
+	do_lcd_command 0b00111000 ; 2x5x7
+	rcall sleep_1ms
+	do_lcd_command 0b00111000 ; 2x5x7
+	do_lcd_command 0b00111000 ; 2x5x7
+	do_lcd_command 0b00001000 ; display off?
+	clear_disp				  ; clear display
+	do_lcd_command 0b00000110 ; increment, no display shift
+	do_lcd_command 0b00001110 ; Cursor on, bar, no blink
+
+	ldi temp, 0b00000000
+	out TCCR0A, temp
+	ldi temp, 0b00000010
+	out TCCR0B, temp
+	ldi temp, 1<<TOIE0
+	sts TIMSK0, temp
+	
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// MAIN
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\\
+
+main:
+	change_line 2, 0			; set to start of line 2
+	write 'F'					; write "Floor Request"
+	write 'l'
+	write 'o'
+	write 'o'
+	write 'r'
+	write ' '
+	write 'R'
+	write 'e'
+	write 'q'
+	write 'u'
+	write 'e'
+	write 's'
+	write 't'
+
+	ldi cmask, INITCOLMASK		; load column mask to scan a column
+	clr col
+
+colloop:
+	cpi col, 4					; check if all columns scanned
+	breq main					; restart scan if all cols scanned
+	sts PORTL, cmask			; scan a column (sts used instead of out since PORTL is in extended I/O space)
+	ldi temp, 0xFF				; slow down scan operation (???? WHY ????)
+
+delay: 
+	dec temp
+	brne delay
+
+	lds floor, PINL				; load current status of PORTL pins (lds must be used instead of in)
+	andi floor, ROWMASK			; and the PINL register with row mask
+	cpi floor, 0xF				; check if any row low
+	breq nextcol				; if temp is all 1s (i.e 0xF), then there are now lows
+								; if there is a low, find which row it is
+	ldi rmask, INITROWMASK		; load Row mask
+	clr row
+
+rowloop:
+	cpi row, 4					; if all rows scanned, jump to next column
+	breq nextcol
+	mov temp2, floor				
+	and temp2, rmask			; mask the input with row mask
+	breq show					; if the bit is clear, a key has been pressed
+								; eg if a key in row 1 is pressed, temp2 = XXXX1101
+								; rmask should equal 00000010
+								; when AND is used, result is 00000000 -> button pressed
+	inc row						; move to next row
+	lsl rmask					; left shift mask to check next row
+	jmp rowloop
+
+nextcol:						; jump to next column when row scan over
+	lsl cmask					; left shift mask to check next col
+	inc col
+	jmp colloop
+
+show:
+	cpi col, 3					; if column = 3, a key in column 3 is pressed, which is a letter key
+	breq n_a					; we dont need to deal with this for this lab, so go to n_a
+
+	cpi row, 3					; if row = 3, a key in row 3 has been pressed, which is any special character or 0
+	breq check_zero				; zero is the only key we are worried about, so go to check_zero
+
+								; formula to convert the coordinates into a number is R*3 + C + 1
+	mov floor, row				; move row to floor
+	lsl floor					; multiply by 2
+	add floor, row				; add row again, to multiply row by 3
+	add floor, col				; add col
+	subi floor, -1				; add 1
+	jmp end_show				
+
+check_zero:						; check if they key pressed in the bottom row is a zero
+	cpi col, 1					; if the button pressed is in column 1, then it is zero
+	brne n_a					; if not, then we dont need to worry about it
+	ldi floor, 10				; clear temp to show 0
+	jmp end_show			
+
+n_a:
+	ser floor					; set all bits in temp to 1 for keys we dont care about
+
+end_show:
+	cpi floor, 10				; check if floor is 10, since 10 requires 2 digits to be printed
+	brne lt10
+
+	rcall show_floor			; set LED bar up 
+
+	ldi floor, 1				; For floor 10, load 1 into floor
+	clear_disp					; clear display initially
+	change_line 2, 14			; set cursor to be after "Floor Request "
+	rcall convert_to_ascii		; convert digit to ascii value
+	write_reg r25				; write the value in r25 onto the LCD
+	
+	ldi floor, 0				; repeat process for 0
+	rcall convert_to_ascii
+	write_reg r25
+	rjmp end_main
+
+lt10:
+	rcall show_floor
+	clear_disp
+	change_line 2, 14
+	rcall convert_to_ascii
+	write_reg r25
+
+end_main:
+	jmp main
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// LCD COMMANDS
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\\
+
+lcd_command:
+	out PORTF, r16
+	rcall sleep_1ms
+	lcd_set LCD_E
+	rcall sleep_1ms
+	lcd_clr LCD_E
+	rcall sleep_1ms
+	ret
+
+lcd_data:
+	out PORTF, r16
+	lcd_set LCD_RS
+	rcall sleep_1ms
+	lcd_set LCD_E
+	rcall sleep_1ms
+	lcd_clr LCD_E
+	rcall sleep_1ms
+	lcd_clr LCD_RS
+	ret
+
+lcd_wait:
+	push r16
+	clr r16
+	out DDRF, r16
+	out PORTF, r16
+	lcd_set LCD_RW
+
+lcd_wait_loop:
+	rcall sleep_1ms
+	lcd_set LCD_E
+	rcall sleep_1ms
+	in r16, PINF
+	lcd_clr LCD_E
+	sbrc r16, 7
+	rjmp lcd_wait_loop
+	lcd_clr LCD_RW
+	ser r16
+	out DDRF, r16
+	pop r16
+	ret
+
+.equ F_CPU = 16000000
+.equ DELAY_1MS = F_CPU / 4 / 1000 - 4
+; 4 cycles per iteration - setup/call-return overhead
+
+sleep_1ms:
+	push r24
+	push r25
+	ldi r25, high(DELAY_1MS)
+	ldi r24, low(DELAY_1MS)
+
+delayloop_1ms:
+	sbiw r25:r24, 1
+	brne delayloop_1ms
+	pop r25
+	pop r24
+	ret
+
+sleep_5ms:
+	rcall sleep_1ms
+	rcall sleep_1ms
+	rcall sleep_1ms
+	rcall sleep_1ms
+	rcall sleep_1ms
+	ret
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// FUNCTIONS
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\\
+// show_floor - input r22 = floor, output X = binary representation
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\\
+
+show_floor:
+;prologue
+;	push YL
+;	push YH
+	push floor
+	push counter
+
+	clr counter	
+	clr XL	; output
+	clr XH
+loop:
+	cp counter, floor
+	breq end_show_floor
+	lsl XL
+	inc XL
+	brcs grtr8
+	rjmp end_x
+grtr8:
+	lsl XH
+	inc XH
+end_x:	
+	inc counter
+	rjmp loop
+
+
+;epilogue
+end_show_floor:
+	out PORTG, XH
+	out PORTC, XL
+
+pop counter
+pop floor
+;pop YH
+;pop YL
+ret
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\\
+// convert_to_ascii - input X = floor in binary, output r7 = ascii val
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\\
+
+convert_to_ascii:
+;prologue
+	push floor
+
+	clr r25
+	mov r25, floor
+	subi r25, -'0'
+
+end_convert:
+;epilogue
+	pop floor
+	ret
--- Lift Project/Assignment V1/Assignment V1/main.asm
+++ Lift Project/Assignment V1/Assignment V1/main.asm
@@ -14,13 +14,24 @@
 Queue_len:
 	.byte 1
 
+/*
+input_array?
+floor_array?
+*/
+
+
 .cseg
 
-.def temp = r16
-.def current_floor = r17
-.def next_floor = r18
-.def input_value = r19
-.def lift_status = r20
+.org 0x0000
+jmp RESET
+
+.def current_floor = r16
+.def next_floor = r17
+.def input_value = r18
+.def lift_status = r19
+.def temp1 = r20
+.def temp2 = r21 //NOTE: temp1 and temp2 will not reliably hold the data given to them
+
 /*
 lift_status is a status register with the bits as flags
 b0 = Stopped? 
@@ -41,97 +52,360 @@ b7 = Halted?
 .equ emergency =	0b01000000
 .equ halted =		0b10000000
 
+//LCD interface constants
+.equ PORTLDIR = 0xF0			; 0xF0 = 0b11110000 -> Setting PORTA 7:4 as output and 3:0 as input
+.equ INITCOLMASK = 0XEF			; 0xEF = 0b11101111 -> Mask to decide which column is selected
+.equ INITROWMASK = 0x01			; 0x01 = 0b00000001 -> Mask to check which row is selected
+.equ ROWMASK = 0x0F				; 0x0F = 0b00001111 -> To get keyboard output value using an AND operation
+.equ LCD_RS = 7
+.equ LCD_E = 6
+.equ LCD_RW = 5
+.equ LCD_BE = 4
+.equ l_one = 0b10000000
+.equ l_two = 0b11000000
 
-.def TimerL = r28
-.def TimerH = r29
+
+
+
+
+
+
+.def TimerL = r28	;YL
+.def TimerH = r29	;YH
+
+
+//Flag checking register  usage check_register_bit stopped
+//								breq action
+.macro check_register_bit
+	mov temp1, lift_status
+	andi temp1, @0
+	cp temp1, @0
+.end macro
+
+//LCD MACROS
+.macro do_lcd_command						// LCD commands
+	ldi temp1, @0
+	rcall lcd_command
+	rcall lcd_wait
+.endmacro
+
+.macro do_lcd_command_reg					// LCD commands, with registers
+	mov temp1, @0
+	rcall lcd_command
+	rcall lcd_wait
+.endmacro
+
+.macro change_line							// change line and cursor position on line
+	ldi temp1, @0
+	cpi temp1, 2
+	breq line_two
+	ldi temp2, l_one
+	ori temp2, @1
+	do_lcd_command_reg temp2
+	jmp end_cl
+line_two:
+	ldi temp2, l_two
+	ori temp2, @1
+	do_lcd_command_reg temp2
+end_cl:
+.endmacro
+
+.macro write								// write immediate data to LCD screen
+	ldi temp1, @0
+	rcall lcd_data
+	rcall lcd_wait
+.endmacro
+
+.macro write_reg							// write register data to LCD screen
+	mov temp1, @0
+	rcall lcd_data
+	rcall lcd_wait
+.endmacro
+
+.macro clear_disp							// clear the LCD Display
+	do_lcd_command 0b00000001
+.endmacro
+
+.macro lcd_set								// set bit in PORTA
+	sbi PORTA, @0
+.endmacro
+.macro lcd_clr								// clear bit in PORTA
+	cbi PORTA, @0
+.endmacro
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Function to insert input floor into list 
-// parameters Address of queue (X), input_floor (r21), current_floor (global), direction (b1 of r20, global)
+// parameters Address of queue (X), input_floor (arg1), current_floor (global), direction (b1 of r20, global)
 // will set next_floor (global), direction (b1 of r20, global)
+
+
 
 insert_request:
 
 //prologue
 
 push SREG
-push temp
+
 push r7
-push r21 //parameter input
-push r22 //local length
+//r16 current_floor global
+//r17 next_floor return value
+push r18	//parameter input_value
+//r19 lift_status global and return value
+push r20	//temp1
+push r22
+
+push XL
+push XH
+
+
+
 
 clr r7 // counter = 0
 
-cp current_floor, r21 //if the current floor is the input floor, break to end
+cp current_floor, input_value //if the current floor is the input floor, break to end
 breq end_insert_request
 lds r22, Queue_len
 
-andi temp, goingUp	// check the goingUp bit
+check_register_bit goingUp	// check the goingUp bit
 breq down_search	// if zero, sort down
 rjmp up_search		//else sort up
 
+//r17 input floor
+//r16 len -->r22
+//r18 counter --> r7
+//r19 ith number
+//r20 current floor
+
+
+//r7 counter r22 len
 
 up_search:
-	cp r17, r20			; if input floor < current floor, jump to up_descending_loop, else up_ascending_loop
+	cp input_value, current_floor			; if input floor < current floor, jump to up_descending_loop, else up_ascending_loop
 	brlt up_descending_loop
 up_ascending_loop:
-	cp r18, r16			; compare counter to len (check if end of list reached)
+	cp r7, r22			; compare counter to len (check if end of list reached)
 	breq end_search
-	ld r19, X			; load floor from output array
-	cp r17, r19			; check if input floor already exists
+	ld temp1, X			; load floor from output array
+	cp r17, temp1			; check if input floor already exists
 	breq end_insert_request	; quit if it does
 	brlo insert_start	; if input floor lower than ith floor, insert 
-	cp r19, r20			; compare ith floor to current floor
+	cp temp1, current_floor			; compare ith floor to current floor
 	brlo insert_start	; if ith < current, insert
 	adiw X, 1			; increment output array
-	inc r18				; increment counter
+	inc r7				; increment counter
 	rjmp up_ascending_loop
 
 up_descending_loop:
-	cp r18, r16			; compare counter to len (check if end of list reached)
+	cp r7, r22			; compare counter to len (check if end of list reached)
 	breq end_search		
-	ld r19, X			; load floor from output array
-	cp r17, r19			; check if input floor already exists
+	ld temp1, X			; load floor from output array
+	cp input_value, temp1			; check if input floor already exists
 	breq end_insert_request	; quit if it does
-	cp r19, r20			; compare ith floor to current floor
+	cp temp1, current_floor			; compare ith floor to current floor
 	brlo down_search	; if input floor < current floor and ith floor < current floor jmp to down search
 	adiw X, 1			; increment output array
-	inc r18				; increment counter
+	inc r7				; increment counter
 	rjmp up_descending_loop
 
 
 
 down_search:
-	cp r20, r17			; compare current floor < input floor
+	cp current_floor, input_value			; compare current floor < input floor
 	brlt down_ascending_loop
 down_descending_loop:
-	cp r18, r16			; compare counter to len (check if end of list reached)
+	cp r7, r22			; compare counter to len (check if end of list reached)
 	breq end_search		
-	ld r19, X			; load floor from output array
-	cp r19, r17			; check if input floor already exists
+	ld temp1, X			; load floor from output array
+	cp temp1, r17			; check if input floor already exists
 	breq end_insert_request	; quit if it does
 	brlo insert_start	; if current floor < input floor insert here
-	cp r20, r19			; compare current floor to ith floor
+	cp current_floor, temp1			; compare current floor to ith floor
 	brlo insert_start	
 	adiw X, 1			; increment output array
-	inc r18				; increment counter
+	inc r7				; increment counter
 	rjmp down_descending_loop
 
 down_ascending_loop:
-	cp r18, r16			; compare counter to len (check if end of list reached)
+	cp r7, r22			; compare counter to len (check if end of list reached)
 	breq end_search		
-	ld r19, X			; load floor from output array
-	cp r19, r17			; check if input floor already exists
+	ld temp1, X			; load floor from output array
+	cp temp1, input_value			; check if input floor already exists
 	breq end_insert_request	; quit if it does
-	cp r20, r19			; compare ith floor to current floor
+	cp current_floor, temp1			; compare ith floor to current floor
 	brlo up_search		; if ith floor > current and input floor > current floor, jmp to upsearch
 	adiw X, 1			; increment output array
-	inc r18				; increment counter
+	inc r7				; increment counter
 	rjmp down_ascending_loop
 
 end_search:
 	rjmp insert_start
 
+insert_start:	
+	inc r22	;len++			;length of list is now longer
+	
 
+insert_loop:
+	cp r22, r7				; comparison of index and list length to check if the end of the list has been reached
+	breq end_insert_loop	
+	st X+, input_value
+	mov input_value, temp1
+	ld temp1, X
+	inc r7
+	rjmp insert_loop
 
+end_insert_loop:
 end_insert_request:
+
+	sts Queue_len, r22		;store new length back in memory
+
+	;epilogue
+	pop XH
+	pop XL
+	pop r22
+	pop r20
+	pop r18
+	pop r7
+	pop SREG
+	ret
+
+
+
+//LCD Functions
+
+lcd_command:
+	out PORTF, r16
+	rcall sleep_1ms
+	lcd_set LCD_E
+	rcall sleep_1ms
+	lcd_clr LCD_E
+	rcall sleep_1ms
+	ret
+
+lcd_data:
+	out PORTF, r16
+	lcd_set LCD_RS
+	rcall sleep_1ms
+	lcd_set LCD_E
+	rcall sleep_1ms
+	lcd_clr LCD_E
+	rcall sleep_1ms
+	lcd_clr LCD_RS
+	ret
+
+lcd_wait:
+	push r16
+	clr r16
+	out DDRF, r16
+	out PORTF, r16
+	lcd_set LCD_RW
+
+lcd_wait_loop:
+	rcall sleep_1ms
+	lcd_set LCD_E
+	rcall sleep_1ms
+	in r16, PINF
+	lcd_clr LCD_E
+	sbrc r16, 7
+	rjmp lcd_wait_loop
+	lcd_clr LCD_RW
+	ser r16
+	out DDRF, r16
+	pop r16
+	ret
+
+.equ F_CPU = 16000000
+.equ DELAY_1MS = F_CPU / 4 / 1000 - 4
+; 4 cycles per iteration - setup/call-return overhead
+
+sleep_1ms:
+	push r24
+	push r25
+	ldi r25, high(DELAY_1MS)
+	ldi r24, low(DELAY_1MS)
+
+delayloop_1ms:
+	sbiw r25:r24, 1
+	brne delayloop_1ms
+	pop r25
+	pop r24
+	ret
+
+sleep_5ms:
+	rcall sleep_1ms
+	rcall sleep_1ms
+	rcall sleep_1ms
+	rcall sleep_1ms
+	rcall sleep_1ms
+	ret
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// FUNCTIONS
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\\
+// show_floor - input r22 = floor, output X = binary representation
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\\
+
+show_floor:
+;prologue
+;	push YL
+;	push YH
+	push floor
+	push counter
+
+	clr counter	
+	clr XL	; output
+	clr XH
+loop:
+	cp counter, floor
+	breq end_show_floor
+	lsl XL
+	inc XL
+	brcs grtr8
+	rjmp end_x
+grtr8:
+	lsl XH
+	inc XH
+end_x:	
+	inc counter
+	rjmp loop
+
+
+;epilogue
+end_show_floor:
+	out PORTG, XH
+	out PORTC, XL
+
+pop counter
+pop floor
+;pop YH
+;pop YL
+ret
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\\
+// convert_to_ascii - input X = floor in binary, output r7 = ascii val
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\\
+
+convert_to_ascii:
+;prologue
+	push floor
+
+	clr r25
+	mov r25, floor
+	subi r25, -'0'
+
+end_convert:
+;epilogue
+	pop floor
+	ret
