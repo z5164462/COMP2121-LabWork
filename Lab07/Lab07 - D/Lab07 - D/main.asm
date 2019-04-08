b907723883110bda67adfa7bb62c78ba2c81fa9a
Anirudh Ramia <anirudhrkl2@gmail.com>
2019-04-08 13:04:32 +1000

    star sign gemini

--- Lab07/Lab07 - D/Lab07 - D/main.asm
+++ Lab07/Lab07 - D/Lab07 - D/main.asm
@@ -6,19 +6,26 @@
 ;
 
 .include "m2560def.inc"
-.def temp = r16
-.def temp2 = r17
+.def temp = r21
+.def temp2 = r22
 .def zero = r3
 .def one = r4
 .def input = r6
 .def floor = r20
-.def direction = r21
-.def req_floor = r22
-.def data = r23
+.def direction = r23
+.def req_floor = r24
+.def data = r25
+.equ l_one = 0b10000000
+.equ l_two = 0b11000000
+.equ PORTLDIR = 0xF0			; 0xF0 = 0b11110000 -> Setting PORTA 7:4 as output and 3:0 as input
+.equ INITCOLMASK = 0XEF			; 0xEF = 0b11101111 -> Mask to decide which column is selected
+.equ INITROWMASK = 0x01			; 0x01 = 0b00000001 -> Mask to check which row is selected
+.equ ROWMASK = 0x0F				; 0x0F = 0b00001111 -> To get keyboard output value using an AND operation
 
-
-.equ PORTLDIR = 0xF0
-
+.def row = r16					; current row
+.def col = r17					; current column
+.def rmask = r18				; mask for row during scan
+.def cmask = r19				; mask for column during scan
 
 .equ clock_speed = 781
 .equ wait_speed = 1
@@ -35,6 +42,33 @@
 	rcall lcd_data
 	rcall lcd_wait
 .endmacro
+
+.macro clear_disp							// clear the LCD Display
+	do_lcd_command 0b00000001
+.endmacro
+
+
+.macro do_lcd_command_reg					// LCD commands, with registers
+	mov r16, @0
+	rcall lcd_command
+	rcall lcd_wait
+.endmacro
+
+.macro change_line							// change line and cursor position on line
+	ldi r16, @0
+	cpi r16, 2
+	breq line_two
+	ldi temp2, l_one
+	ori temp2, @1
+	do_lcd_command_reg temp2
+	jmp end_cl
+line_two:
+	ldi temp2, l_two
+	ori temp2, @1
+	do_lcd_command_reg temp2
+end_cl:
+.endmacro
+
 
 .macro write								// write immediate data to LCD screen
 	ldi r16, @0
@@ -293,7 +327,7 @@ End_I:
 
 //-------------------------------------------------------------
 
-
+//
 
 main:
 	ldi floor, 1 ;this is the FlooR <-------------------------------
@@ -314,22 +348,119 @@ main:
 	mov temp, floor
 	subi temp, -48
 	do_lcd_data temp
+//////////////////////////////////////////////////////////////////////////////
+	ldi cmask, INITCOLMASK		; load column mask to scan a column
+	clr col
+
+colloop:
+	cpi col, 4					; check if all columns scanned
+	breq main					; restart scan if all cols scanned
+	sts PORTL, cmask			; scan a column (sts used instead of out since PORTL is in extended I/O space)
+	ldi temp, 0xFF				; slow down scan operation (???? WHY ????)
+
+delay: 
+	dec temp
+	brne delay
+
+	lds floor, PINL					; load current status of PORTL pins (lds must be used instead of in)
+	andi floor, ROWMASK			; and the PINL register with row mask
+	cpi floor, 0xF				; check if any row low
+	breq nextcol				; if temp is all 1s (i.e 0xF), then there are now lows
+								; if there is a low, find which row it is
+	ldi rmask, INITROWMASK		; load Row mask
+	clr row
+
+rowloop:
+	cpi row, 4					; if all rows scanned, jump to next column
+	breq nextcol
+	mov temp2, floor				
+	and temp2, rmask			; mask the input with row mask
+	breq show					; if the bit is clear, a key has been pressed
+								; eg if a key in row 1 is pressed, temp2 = XXXX1101
+								; rmask should equal 00000010
+								; when AND is used, result is 00000000 -> button pressed
+	inc row						; move to next row
+	lsl rmask					; left shift mask to check next row
+	jmp rowloop
+
+nextcol:						; jump to next column when row scan over
+	lsl cmask					; left shift mask to check next col
+	inc col
+	jmp colloop
+
+show:
+	cpi col, 3					; if column = 3, a key in column 3 is pressed, which is a letter key
+	breq n_a					; we dont need to deal with this for this lab, so go to n_a
+
+	cpi row, 3					; if row = 3, a key in row 3 has been pressed, which is any special character or 0
+	breq check_bot				; zero is the only key we are worried about, so go to check_zero
+
+								; formula to convert the coordinates into a number is R*3 + C + 1
+	mov floor, row				; move row to floor
+	lsl floor					; multiply by 2
+	add floor, row				; add row again, to multiply row by 3
+	add floor, col				; add col
+	subi floor, -1				; add 1
+	jmp end_show				
+
+check_bot:						; check if they key pressed in the bottom row is a zero
+	cpi col, 1					; if the button pressed is in column 1, then it is zero
+	brne star					; if not, then we dont need to worry about it
+	ldi floor, 10				; clear temp to show 0
+	jmp end_show			
+
+star:
+	cpi col, 0
+	brne n_a
+	ldi floor, 1
+	jmp end_show
+
+n_a:
+	ser floor					; set all bits in temp to 1 for keys we dont care about
+
+end_show:
+	cpi floor, 10				; check if floor is 10, since 10 requires 2 digits to be printed
+	brne lt10
+
+	rcall show_floor			; set LED bar up 
+
+	ldi floor, 1				; For floor 10, load 1 into floor
+	clear_disp					; clear display initially
+	change_line 2, 14			; set cursor to be after "Floor Request "
+	rcall convert_to_ascii		; convert digit to ascii value
+	write_reg r25				; write the value in r25 onto the LCD
+	
+	ldi floor, 0				; repeat process for 0
+	rcall convert_to_ascii
+	write_reg r25
+	rjmp end_main
+
+lt10:
+	rcall show_floor
+	clear_disp
+	change_line 2, 14
+	rcall convert_to_ascii
+	write_reg r25
+
+
+	//////////////////////////////////////////////////////////
+
+
+
+
 
 wait_loop:
 
-	rcall Keypad_input
+/*	//rcall Keypad_input
 	ldi temp, star
 	cp input, temp
 	brne check_count
 	write 'A'
-/*	lds r24, Seconds
+	lds r24, Seconds
 	lds r25, Seconds+1
 	adiw r25:r24, 1
 	sts Seconds, r24
 	sts Seconds+1, r25*/
-
-
-
 
 check_count:
 
@@ -422,19 +553,16 @@ end_main:
 ;input r20 = floor, output X = binary representation
 show_floor:
 ;prologue
-	push YL
-	push YH
+;	push YL
+;	push YH
 	push floor
-	push r15
-	push XL
-	push XH
+	push r16
 
-
-	clr r15	; counter
+	clr r16	
 	clr XL	; output
 	clr XH
 loop:
-	cp r15, floor
+	cp r16, floor
 	breq end_show_floor
 	lsl XL
 	inc XL
@@ -444,7 +572,7 @@ grtr8:
 	lsl XH
 	inc XH
 end_x:	
-	inc r15
+	inc r16
 	rjmp loop
 
 
@@ -453,13 +581,28 @@ end_show_floor:
 	out PORTG, XH
 	out PORTC, XL
 
-pop XH
-pop XL
-pop r15
+pop r16
 pop floor
-pop YH
-pop YL
+;pop YH
+;pop YL
 ret
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\\
+// convert_to_ascii - input X = floor in binary, output r7 = ascii val
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\\
+
+convert_to_ascii:
+;prologue
+	push floor
+
+	clr r25
+	mov r25, floor
+	subi r25, -'0'
+
+end_convert:
+;epilogue
+	pop floor
+	ret
 
 
 //LCD-example functions
@@ -551,7 +694,7 @@ sleep_5ms:
 
 
 
-
+/*
 .equ COL0 = 0xEF
 .equ COL1 = 0b11011111
 .equ COL2 = 0b10111111
@@ -729,4 +872,4 @@ Keypad_end:
 	out SREG, temp2
 	pop temp2
 	pop temp
-	ret
+	ret*/
